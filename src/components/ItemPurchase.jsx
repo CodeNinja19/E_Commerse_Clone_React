bdac9e413c9c0ee594f1e2924d10796ae41de5f8
CodeNinja19 <kartiks9999@gmail.com>
2024-03-23 19:18:52 +0530

    shows when items is added to the cart on the purchase page

--- src/components/ItemPurchase.jsx
+++ src/components/ItemPurchase.jsx
@@ -3,42 +3,52 @@ import { ProductData } from "../Context/ProductData";
 import { useContext, useState, useEffect } from "react"
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import {NavLink} from "react-router-dom"
+import Alert from '@mui/material/Alert';
+import { NavLink } from "react-router-dom"
+import { motion as m, AnimatePresence } from "framer-motion"
+import { Router } from "react-router-dom";
 
 export default function ItemPurchase() {
 
     const [purchase, setPurchase] = useContext(ProductOfInterest);
-    const [prodQuantity,setProdQuantity]=useState(1);
-    const [data,setData]=useContext(ProductData);
+    const [prodQuantity, setProdQuantity] = useState(1);
+    const [data, setData] = useContext(ProductData);
+    const [itemAlert, setItemAlert] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         setProdQuantity(purchase.quantity)
-    },[purchase])
+    }, [purchase])
 
-    useEffect(()=>{
-        for (let e of data){
-            if (e.id===purchase.id){
+    useEffect(() => {
+        for (let e of data) {
+            if (e.id === purchase.id) {
                 setPurchase(e);
             }
         }
-    },[data])
+    }, [data])
 
-    const changeQuantity=function(){
-        if (prodQuantity==="") {setProdQuantity(0);}
-        else{
-            setPurchase({...purchase,quantity:prodQuantity})
-            setData(data.map((e)=>{
-                if (e.id===purchase.id){ return  {...e,quantity:prodQuantity}}
-                else {return e}
+    const changeQuantity = function () {
+        if (prodQuantity === "") { setProdQuantity(0); }
+        else {
+            manageAlert();
+            setPurchase({ ...purchase, quantity: prodQuantity })
+            setData(data.map((e) => {
+                if (e.id === purchase.id) { return { ...e, quantity: prodQuantity } }
+                else { return e }
             }))
         }
     }
-    const typing =function (e){
-        const newValue=e.target.value;
-        if (parseInt(newValue)!=NaN && newValue>=0 && newValue<=100) {
+    const typing = function (e) {
+        const newValue = e.target.value;
+        if (parseInt(newValue) != NaN && newValue >= 0 && newValue <= 100) {
             setProdQuantity(newValue);
         }
 
+    }
+
+    const manageAlert = function () {
+        setItemAlert(true);
+        setTimeout(() => (setItemAlert(false)), 2000);
     }
 
     return (
@@ -53,18 +63,33 @@ export default function ItemPurchase() {
                     ame.</p>
                 <div className="flex">
                     <div className="flex h-fit border-b border-slate-300 w-full pb-3">
-                        <input type="text" value={prodQuantity} className="w-12 text-center" onChange={typing}/>
+                        <input type="text" value={prodQuantity} className="w-12 text-center" onChange={typing} />
                         <div className="flex flex-col items-center bg-white">
-                        <h1 className="active:bg-slate-200 duration-500"><ArrowDropUpIcon sx={{fontSize:"1.3rem",m:"0"}} onClick={()=>{setProdQuantity(e=>(e+1))}}/></h1>
-                        <h1 className="active:bg-slate-200 duration-500"><ArrowDropDownIcon sx={{fontSize:"1.3rem",m:"0"}} onClick={()=>{setProdQuantity(e=>{if(prodQuantity!=0){return e-1} else {return e}})}}/></h1>
+                            <h1 className="active:bg-slate-200 duration-500"><ArrowDropUpIcon sx={{ fontSize: "1.3rem", m: "0" }} onClick={() => { setProdQuantity(e => (e + 1)) }} /></h1>
+                            <h1 className="active:bg-slate-200 duration-500"><ArrowDropDownIcon sx={{ fontSize: "1.3rem", m: "0" }} onClick={() => { setProdQuantity(e => { if (prodQuantity != 0) { return e - 1 } else { return e } }) }} /></h1>
                         </div>
                         <button className="ml-8 bg-[#6a9739] hover:opacity-75 duration-500 px-20 h-fit py-2 text-center text-white text-md font-semibold rounded my-auto" onClick={changeQuantity}>Add To Cart</button>
                     </div>
                 </div>
-                    <div className="flex space-x-2">
-                        <h1>Category:</h1>
-                        <NavLink to={`/${purchase.category}`} className={({isActive})=>{return isActive?"text-[#6a9739]":"text-slate-500";}}><h1>{purchase.category}</h1></NavLink>
-                    </div>
+                <div className="flex space-x-2">
+                    <h1>Category:</h1>
+                    <NavLink to={`/${purchase.category}`} className={({ isActive }) => { return isActive ? "text-[#6a9739]" : "text-slate-500"; }}><h1>{purchase.category}</h1></NavLink>
+                </div>
+                <AnimatePresence>
+                    {itemAlert &&
+                        <m.div
+                            key={Router.pathName}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ duration: 0.6, ease: "easeOut" }}
+                        >
+                            <Alert variant="filled" severity="success">
+                                The Item has been added to the cart
+                            </Alert>
+                        </m.div>
+                    }
+                </AnimatePresence>
             </div>
         </div>
     )
